ef421d8e888c31d9cf0c3d95009efd3c3832465a
winthihasoe <54246971+winthihasoe@users.noreply.github.com>
2022-06-20 23:14:49 +0630

    testing for remote access on git

--- helpers/axiosConfig.js
+++ helpers/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
+  //Testing for all available methods which can access localhost
   // baseURL: "http://10.0.2.2:8000/api",
   baseURL: "https://genevacaring.com/api/v1",
   // baseURL: "http://localhost:8000/api/v1",
