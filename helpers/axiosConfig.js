f68528c1bf2ffa355a7c53be49ed43c190ca4774
winthihasoe <54246971+winthihasoe@users.noreply.github.com>
2022-07-20 21:44:44 +0630

    before build android app

--- helpers/axiosConfig.js
+++ helpers/axiosConfig.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
+  baseURL: "https://a2medicalsupply.com/api/v1",
   //Testing for all available methods which can access localhost
-  // baseURL: "http://10.0.2.2:8000/api",
-  baseURL: "https://genevacaring.com/api/v1",
+  // baseURL: "http://10.0.2.2:8000/api/v1",
+  // baseURL: "https://genevacaring.com/api/v1",
   // baseURL: "http://localhost:8000/api/v1",
 });
 
