e61d8d6e0e9c348897b7bb8eedf7f9990601e657
winthihasoe <54246971+winthihasoe@users.noreply.github.com>
2022-08-30 08:11:20 +0630

    fontsize change in ShowDrug

--- routes/ShowDrug.js
+++ routes/ShowDrug.js
@@ -1,56 +1,64 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 
 export default function ShowDrug({ route, navigation }) {
   const item = route.params;
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          fontSize: 20,
-          fontWeight: "bold",
-          marginBottom: 20,
-          textAlign: "center",
-        }}
-      >
-        {item.drug_name}
-      </Text>
-      <View style={styles.row}>
-        <Text style={styles.label}>Retail Price: </Text>
-        <Text style={styles.label}>{item.retail_price} ks</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Doctor Price: </Text>
-        <Text style={styles.label}>{item.dr_price} ks</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Buying Price: </Text>
-        <Text style={styles.label}>{item.buying_price} ks</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Supplier: </Text>
-        <Text style={styles.label}>{item.supplier}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Purchased at: </Text>
-        <Text style={styles.label}>{item.purchase_date}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Created at: </Text>
-        <Text style={styles.label}>{item.created_at.slice(0, 10)}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Updated at: </Text>
-        <Text style={styles.label}>{item.updated_at.slice(0, 10)}</Text>
-      </View>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => navigation.navigate("EditDrug", item)}
-      >
-        <Text style={{ color: "white", fontSize: 18, textAlign: "center" }}>
-          Edit
+    <ScrollView>
+      <View style={styles.container}>
+        <Text
+          style={{
+            fontSize: 17,
+            fontWeight: "bold",
+            marginBottom: 10,
+            textAlign: "center",
+          }}
+        >
+          {item.drug_name}
         </Text>
-      </TouchableOpacity>
-    </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Retail Price: </Text>
+          <Text style={styles.label}>{item.retail_price} ks</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Doctor Price: </Text>
+          <Text style={styles.label}>{item.dr_price} ks</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Purchased Price: </Text>
+          <Text style={styles.label}>{item.buying_price} ks</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Supplier: </Text>
+          <Text style={styles.label}>{item.supplier}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Purchased at: </Text>
+          <Text style={styles.label}>{item.purchase_date}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Created at: </Text>
+          <Text style={styles.label}>{item.created_at.slice(0, 10)}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Updated at: </Text>
+          <Text style={styles.label}>{item.updated_at.slice(0, 10)}</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => navigation.navigate("EditDrug", item)}
+        >
+          <Text style={{ color: "white", fontSize: 18, textAlign: "center" }}>
+            Edit
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -69,14 +77,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    borderWidth: 1,
+    borderWidth: 0.8,
     borderColor: "#bbb",
-    padding: 8,
+    padding: 4,
     marginBottom: 2,
   },
   label: {
-    lineHeight: 30,
-    fontSize: 16,
+    lineHeight: 25,
+    fontSize: 14,
     fontStyle: "italic",
   },
   btn: {
